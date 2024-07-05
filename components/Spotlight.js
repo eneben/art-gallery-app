e6a971d80ce46a7a401f141e97a55ee1f056e31c
Susan Rahmawati <susanrahma98@gmail.com>
2024-07-05 15:09:10 +0200

    add Spotlight Piece random

---
+++ components/Spotlight.js
@@ -0,0 +1,25 @@
+import Image from "next/image";
+import styled from "styled-components";
+
+export default function Spotlight({ image, artist }) {
+  const SpotlightComponent = styled.div`
+    position: relative;
+    height: 300px;
+  `;
+
+  const StyleHeading = styled.h1``;
+  return (
+    <>
+      <SpotlightComponent>
+        <StyleHeading>Art GALLERY</StyleHeading>
+        <Image
+          src={image}
+          alt={`by ${artist}`}
+          fill
+          style={{ objectFit: "contain" }}
+        />
+      </SpotlightComponent>
+      <figcaption>{` by ${artist}`}</figcaption>
+    </>
+  );
+}
