d98009642d18b9ede96c0daad80f1e61e0f0da25
Elisabeth Neben <elisabeth.neben@posteo.de>
2024-07-05 10:45:10 +0200

    begin with user story 1

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,21 @@
+import useSWR from "swr";
+
+const URL = "https://example-apis.vercel.app/api/art";
+
+const fetcher = async (url) => {
+  const response = await fetch(url);
+  if (!response.ok) {
+    const error = new Error("An error occured while fetching data.");
+    error.info = await response.json();
+    error.status = response.status;
+    throw error;
+  }
+  return response.json();
+};
+
 export default function HomePage() {
+  const { data, isLoading, error } = useSWR(URL, fetcher);
+
   return (
     <div>
       <h1>Hello from Next.js</h1>
