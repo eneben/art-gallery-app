91f0af4ed03be38a840309769df252f69518f386
Elisabeth Neben <elisabeth.neben@posteo.de>
2024-07-08 13:04:44 +0200

    refactoring, delete unneccessary component

--- pages/art-pieces/index.js
+++ pages/art-pieces/index.js
@@ -1,5 +1,23 @@
-import ArtPiecesList from "@/components/ArtPieces";
+import ArtPiecesPreview from "./ArtPiecesPreview";
+import styled from "styled-components";
 
-export default function ArtPiecesPage({ pieces }) {
-  return <ArtPiecesList pieces={pieces} />;
+export default function ArtPieces({ pieces }) {
+  const ListItem = styled.li`
+    list-style: none;
+  `;
+
+  return (
+    <>
+      <h1>List of all Art Pieces</h1>
+      <ul>
+        {pieces.map((piece) => {
+          return (
+            <ListItem key={piece.slug}>
+              <ArtPiecesPreview piece={piece} />
+            </ListItem>
+          );
+        })}
+      </ul>
+    </>
+  );
 }
