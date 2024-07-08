ad4c46cc7da3a08c1d705d368f7971a470badaa0
Elisabeth Neben <elisabeth.neben@posteo.de>
2024-07-08 15:09:20 +0200

    add functionality to copy the color to the clipboard

--- components/ArtPieceDetails.js
+++ components/ArtPieceDetails.js
@@ -1,7 +1,10 @@
 import Image from "next/image";
+import { useState, useEffect } from "react";
 import styled from "styled-components";
 
 export default function Details({ piece }) {
+  const [isCopied, setIsCopied] = useState(false);
+
   const {
     imageSource: image,
     name: title,
@@ -31,7 +34,30 @@ export default function Details({ piece }) {
     width: 40px;
     border-radius: 50%;
     background-color: ${(props) => props.color};
+    cursor: pointer;
+
+    &:hover {
+      opacity: 0.8;
+    }
   `;
+
+  async function handleColorClick(color) {
+    try {
+      await navigator.clipboard.writeText(color);
+      setIsCopied(true);
+    } catch (error) {
+      console.error("An error occurred:", error.message);
+    }
+  }
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setIsCopied(false);
+    }, 3000);
+    return () => {
+      clearInterval(timer);
+    };
+  }, [isCopied]);
 
   return (
     <>
@@ -44,15 +70,27 @@ export default function Details({ piece }) {
         />
       </StyledContainer>
       <h1>{`${artist}: "${title}"`}</h1>
-      <StyledColorSection>
-        {colors.map((color) => {
-          return <StyledColorCircle key={color} color={color} />;
-        })}
-      </StyledColorSection>
       <StyledList>
         <li>year: {year}</li>
         <li>genre: {genre}</li>
+        <li>colors:</li>
       </StyledList>
+      <StyledColorSection>
+        {colors.map((color) => {
+          return (
+            <StyledColorCircle
+              key={color}
+              color={color}
+              onClick={() => handleColorClick(color)}
+            />
+          );
+        })}
+      </StyledColorSection>
+      <p>
+        {isCopied
+          ? "The hex value of the clicked color is copied to the clipboard."
+          : "By clicking on a color you can copy it to the clipboard."}
+      </p>
     </>
   );
 }
