897fb1dbbe9f37d219759271097a226a9f7b5faa
Elisabeth Neben <elisabeth.neben@posteo.de>
2024-07-08 11:42:33 +0200

    put the fetching logic in the app.js and add a navigation-bar. just the logic, not the styling.

---
+++ components/Navigation.js
@@ -0,0 +1,27 @@
+import Link from "next/link";
+import styled from "styled-components";
+
+export default function Navigation() {
+  const StyledList = styled.ul`
+    display: flex;
+    justify-content: space-evenly;
+  `;
+
+  const ListItem = styled.li`
+    list-style: none;
+  `;
+
+  return (
+    <ul>
+      <ListItem>
+        <Link href="/">Spotlight</Link>
+      </ListItem>
+      <ListItem>
+        <Link href="/art-pieces">Art Pieces</Link>
+      </ListItem>
+      <ListItem>
+        <Link href="/favorites">Favorites</Link>
+      </ListItem>
+    </ul>
+  );
+}
