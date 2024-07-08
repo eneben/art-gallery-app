ed6dd3e8ecd9b2f360a3461d61d3607c17ec9c66
Elisabeth Neben <elisabeth.neben@posteo.de>
2024-07-08 14:45:45 +0200

    add color circles to detail page

--- components/ArtPieceDetails.js
+++ components/ArtPieceDetails.js
@@ -2,7 +2,14 @@ import Image from "next/image";
 import styled from "styled-components";
 
 export default function Details({ piece }) {
-  const { imageSource: image, name: title, artist, year, genre } = piece;
+  const {
+    imageSource: image,
+    name: title,
+    artist,
+    year,
+    genre,
+    colors,
+  } = piece;
 
   const StyledContainer = styled.div`
     position: relative;
@@ -11,6 +18,19 @@ export default function Details({ piece }) {
 
   const StyledList = styled.ul`
     list-style: none;
+  `;
+
+  const StyledColorSection = styled.ul`
+    display: flex;
+    gap: 10px;
+    list-style: none;
+  `;
+
+  const StyledColorCircle = styled.li`
+    height: 40px;
+    width: 40px;
+    border-radius: 50%;
+    background-color: ${(props) => props.color};
   `;
 
   return (
@@ -24,6 +44,11 @@ export default function Details({ piece }) {
         />
       </StyledContainer>
       <h1>{`${artist}: "${title}"`}</h1>
+      <StyledColorSection>
+        {colors.map((color) => {
+          return <StyledColorCircle key={color} color={color} />;
+        })}
+      </StyledColorSection>
       <StyledList>
         <li>year: {year}</li>
         <li>genre: {genre}</li>
